d01a3b8fd0ce83d71fa11ece30bc07c1ece2c12a
wqyouzhifu <wqyouzhifu@163.com>
2018-05-06 15:23:45 +0800

    提交demo.sh文件

---
+++ demo.sh
@@ -0,0 +1,20 @@
+cd ~/Desktop
+if [ -d $1 ]; then
+  echo 'error: dir exists'
+  exit
+else
+  mkdir $1
+  cd $1
+  mkdir css js
+  touch index.html css/style.css js/main.js
+  echo '  <!DOCTYPE>
+ <link rel='stylesheet' href='./css/style.css'>
+ <script src='./js/main.js'></script>
+ <title>Hello</title>
+ <h1>Hi</h1>'>>index.html
+ echo 'h1{color: red;}'>>css/style.css
+ echo ' var string = "Hello World"
+ alert(string)'>>js/main.js
+  echo 'success'
+  exit
+fi
